e732f984650265a618b98997ebc10c29cf530cb1
KevinZucca <kev.94@live.it>
2023-03-08 12:59:54 +0100

    feat: creating the array with objects in
    printing the infos in console

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// 1) Creating a new array of object with teams informations
+// 2) Print on console for each member the relative informations, includings photo like a string
+// 3) Print the same info on DOM like strings
+
+// 4) Edit the string "photo" in an image
+// 5) Treat each member like a card
+
+
+
+const ourTeam = [
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        mansion: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela",
+        surname: "Caroll",
+        mansion: "Chief Editor",
+        photo:  "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter",
+        surname: "Gordon",
+        mansion: "Office Manager",
+        photo:  " walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela",
+        surname: "Lopez",
+        mansion: "Social Media Manager",
+        photo:  "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott",
+        surname: "Estrada",
+        mansion: "Developer",
+        photo:  "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara",
+        surname: "Ramos",
+        mansion: "Graphic Designer",
+        photo:  "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+console.log(ourTeam[0]);
+console.log(ourTeam[1]);
+console.log(ourTeam[2]);
+console.log(ourTeam[3]);
+console.log(ourTeam[4]);
+console.log(ourTeam[5]);
+
+
+
+
